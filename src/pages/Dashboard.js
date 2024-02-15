6aa03cf4cd5c163b4b1439c3204319f4e833b3ee
Adesina Adebisi <104420789+AdebisiRex@users.noreply.github.com>
2024-02-15 08:46:20 +0100

    api requests class using use-effect

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,18 @@
-import React from 'react'
-
-import PrivateNav from '../components/PrivateNav';
+import React, { useEffect } from "react";
+import axios from "axios";
+import PrivateNav from "../components/PrivateNav";
 const Dashboard = () => {
+  const fetchCocktails = async () => {
+    const response = await axios.get(
+      "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+    );
+    console.log(response.data);
+  };
+  useEffect(() => {
+    fetchCocktails();
+  }, []);
   return (
     <>
-
       <div className='bg- min-vh-100 d-flex align-items-center justify-content-center'>
         <div className='text-center text-success'>
           <h1>Welcome to the dashboard </h1>
@@ -13,6 +21,6 @@ const Dashboard = () => {
       </div>
     </>
   );
-}
+};
 
-export default Dashboard
+export default Dashboard;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Nav from '../components/Nav';
+import axios from 'axios';
 
 const LandingPage = () => {
+  const fetchCocktails= async()=>{
+    const response = await axios.get(
+      "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+    );
+    console.log(response.data)
+  }
   return (
     <>
     <Nav/>
     <div className='App'>
       <header className='App-header'>
         <h1> Landing Page</h1>
+        <button onClick={fetchCocktails}>Fetch</button>
       </header>
     </div>
     </>
